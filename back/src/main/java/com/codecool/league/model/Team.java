b3d97a7a249e10597d3895eb08621bea9cdb2dc1
MadalinCiobanu <ciobanumadalinflorin@gmail.com>
2021-05-25 11:23:32 +0300

    add team model

---
+++ back/src/main/java/com/codecool/league/model/Team.java
@@ -0,0 +1,26 @@
+package com.codecool.league.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class Team {
+
+    @Id
+    @SequenceGenerator(name = "team_sequence", sequenceName = "team_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "team_sequence")
+    private long id;
+
+    @NotNull @Size(min = 3, max = 20)
+    private String name;
+
+    @OneToMany
+    @JsonIgnoreProperties(value = {"team"})
+    private List<User> players;
+
+}
