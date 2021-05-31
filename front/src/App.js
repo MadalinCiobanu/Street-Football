dce3b8e587754c276c1703527927c4f266b6ed04
MadalinCiobanu <ciobanumadalinflorin@gmail.com>
2021-05-31 10:18:02 +0300

    update packages and app design

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Header />
+        <Route exact path="/" component={Home}/>
+        <Footer />
+      </Router>
     </div>
   );
 }
