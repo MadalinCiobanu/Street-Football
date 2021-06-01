717eb2ca5af660632d21a69dd6382e083ef7547f
MadalinCiobanu <ciobanumadalinflorin@gmail.com>
2021-06-01 13:03:03 +0300

    add home component

---
+++ front/src/components/Home.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link, useHistory } from "react-router-dom";
+
+export default function Home() {
+
+    const history = useHistory();
+
+    return (
+        <div>
+            <div className="bg">
+                <div>
+                    <p className="title">Join our community</p>
+                    <button className="button is-light is-medium"
+                    onClick={() => history.push("/register")}>
+                        Register Now
+                    </button>
+                </div>
+            </div>
+            <div className="card-container">
+                <Link to="/team" className="card card1">
+                    <h1 className="card-text">
+                        Join a Team
+                    </h1>
+                </Link>
+                <Link to="/referee" className="card card2">
+                    <h1 className="card-text">
+                        Play with Referees
+                    </h1>
+                </Link>
+                <Link to="/tournament" className="card card3">
+                    <h1 className="card-text">
+                        Win Tournaments
+                    </h1>
+                </Link>
+            </div>
+        </div>
+    )
+}
