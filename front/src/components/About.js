1a60896d1d0363daca0eebc2155d9ddfd1c1ff75
MadalinCiobanu <ciobanumadalinflorin@gmail.com>
2021-06-08 14:33:30 +0300

    add about component

---
+++ front/src/components/About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function About() {
+    return (
+        <div className="form-container">
+            <div className="about">
+                <h1 className="box-text margin-bot">For Football Fans, by Football Fan</h1>
+                <p>
+                    Made by a football fan for football fans, Street Football is an web application
+                    that make it easy to play the sport we love in a professional manner.
+                </p>
+                <p>
+                    You can easily register, join or create a team and start playing with other teams,
+                    or even join tournaments to win cups and glory.
+                </p>
+                <p>
+                The idea of ​​the application started in 2021 when our founder, Madalin Ciobanu, combined 
+                his two passions, football and coding and created this application in the last part of 
+                his learning process at the Codecool Romania programming course.
+                </p>
+            </div>
+        </div>
+    )
+}
