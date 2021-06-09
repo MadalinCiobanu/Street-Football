c4e191128ea6dd30ac7950965a76f13a9afe747d
MadalinCiobanu <ciobanumadalinflorin@gmail.com>
2021-06-09 14:07:22 +0300

    add comingsoon component

---
+++ front/src/components/ComingSoon.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function ComingSoon() {
+    return (
+        <div className="form-container">
+            <div className="about">
+                <h1 className="box-text">Coming Soon</h1>
+            </div>
+        </div>
+    )
+}
