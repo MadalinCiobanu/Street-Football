9a83ac44a7ba3fa0ecd54d7889b3a2546fa2b4b7
MadalinCiobanu <ciobanumadalinflorin@gmail.com>
2021-06-16 23:23:34 +0300

    add passwordChanger component

---
+++ front/src/components/userDetails/PasswordChanger.js
@@ -0,0 +1,74 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function PasswordChanger() {
+
+    const email = localStorage.getItem("email");
+
+    const [password, setPassword] = useState({
+        old: "",
+        password: "",
+        confirmPassword: ""
+    })
+
+    const [errors, setErrors] = useState({});
+
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    const handlePassword = (e) => {
+        setPassword({
+            ...password,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const validate = (password) => {
+
+        let errors = {};
+
+        if(password.old.length < 5 || password.old.length > 20) {
+            errors.old = "Invalid password";
+        }
+
+        if (password.password.length < 5 || password.password.length > 20) {
+            errors.password = "Invalid password (size: 5-20)";
+        }
+    
+        if (!(password.password === password.confirmPassword)) {
+            errors.confirmPassword = "Invalid password";
+        }
+
+        return errors;
+
+    }
+    
+    const handleSubmitPassword = (e) => {
+        e.preventDefault();
+        // setErrors(validate(values));
+        setErrors(validate(password));
+        console.log(errors);
+        console.log(password);
+        setIsSubmitted(true);
+    };
+
+    useEffect(() => {
+        console.log(errors);
+        if (Object.keys(errors).length === 0 && isSubmitted) {
+            console.log("in useEffect");
+            axios.put(`http://localhost:8080/user/${email}`, password, {
+                headers: {
+                Authorization: `Bearer ${window.localStorage.getItem("token")}`,
+                }
+            })
+            .then( res => {
+                if (res.status === 200) {
+                console.log("ok")
+                }
+            }, () => {
+                setErrors({old: "Wrong password"});
+            });
+        }
+    }, [errors])
+
+    return { password, handlePassword, handleSubmitPassword, errors };
+}
