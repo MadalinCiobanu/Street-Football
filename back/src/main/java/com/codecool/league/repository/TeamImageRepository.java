bfb8ffbd099cb4958506cb42b5c031dfea34d0f3
MadalinCiobanu <ciobanumadalinflorin@gmail.com>
2021-06-24 20:49:39 +0300

    add team-image repository

---
+++ back/src/main/java/com/codecool/league/repository/TeamImageRepository.java
@@ -0,0 +1,7 @@
+package com.codecool.league.repository;
+
+import com.codecool.league.model.TeamImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamImageRepository extends JpaRepository<TeamImage, Long> {
+}
